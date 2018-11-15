ccab8aae9bbf82f99911119146c881dd38f3c88c
DrunkenOwl <44969326+DeunkenOwl@users.noreply.github.com>
2018-11-15 21:37:55 +1000

    Сдача задания

---
+++ summa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число : ");
+            string str = Console.ReadLine();
+            int a = Convert.ToInt32(str);
+            Console.Write("Введите второе число : ");
+            str = Console.ReadLine();
+            int b = Convert.ToInt32(str);
+            a = a + b;
+            Console.Write("Сумма : ");
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
